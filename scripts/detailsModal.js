0ffa97d0f0f129425775680c1d90437f05d493e0
ditod1 <orlandodiazconde@gmail.com>
2023-03-15 07:10:44 -0500

    Change language

--- scripts/detailsModal.js
+++ scripts/detailsModal.js
@@ -2,32 +2,65 @@ const works = document.querySelector('#portfolio');
 const workTemplate = document.querySelector('#work-template');
 const modal = document.querySelector('.hide-modal');
 
+// const projectLibrary = [
+//   {
+//     name: 'Space Travelers',
+//     image: '../images/portfolio/spacetravelers.png',
+//     description: 'SPA that shows up-to-date data on Space-X missions and rockets.',
+//     tech: ['React', 'Redux', 'Redux Toolkit', 'Bootstrap'],
+//     liveLink: 'https://lighthearted-sfogliatella-77e09a.netlify.app/',
+//     sourceLink: 'https://github.com/orlandodiazc/event-reservations',
+//   },
+//   {
+//     name: 'Mathematicians',
+//     image: '../images/portfolio/mathematicians.png',
+//     description:
+//       'Single Page Application that allows the user to make quick calculations, it also has welcome and quote pages',
+//     tech: ['React', 'Jest', 'Bootstrap', 'React Testing Library'],
+//     liveLink: 'https://effulgent-douhua-34f1cc.netlify.app/',
+//     sourceLink: 'https://github.com/orlandodiazc/mathematicians',
+//   },
+//   {
+//     name: 'Todo List',
+//     image: '../images/portfolio/todolist.png',
+//     description:
+//       'To do list is JavaScript project that allows you to add, delete and edit your daily tasks.',
+//     tech: ['Javascript', 'CSS', 'HTML5', 'Webpack'],
+//     liveLink: 'https://stunning-bonbon-dbeffd.netlify.app/',
+//     sourceLink: 'https://github.com/orlandodiazc/to-do-list',
+//   },
+// ];
+
 const projectLibrary = [
   {
     name: 'Space Travelers',
     image: '../images/portfolio/spacetravelers.png',
-    description: 'SPA that shows up-to-date data on Space-X missions and rockets',
+    description:
+      'Aplicación de una página que permite explorar las próximas misiones y cohetes de SpaceX, además podrás seleccionar a cuales desea entrar o reservar.',
     tech: ['React', 'Redux', 'Redux Toolkit', 'Bootstrap'],
+    liveLink: 'https://lighthearted-sfogliatella-77e09a.netlify.app/',
+    sourceLink: 'https://github.com/orlandodiazc/event-reservations',
   },
   {
     name: 'Mathematicians',
-    image: '../images/portfolio/spacetravelers.png',
+    image: '../images/portfolio/mathematicians.png',
     description:
-      'Single Page Application that allows the user to make quick calculations, it also has welcome and quote pages',
+      'Website que permite al usuario hacer cálculos matemáticos rápidos, además tiene páginas de bienvenida y frases.',
     tech: ['React', 'Jest', 'Bootstrap', 'React Testing Library'],
     liveLink: 'https://effulgent-douhua-34f1cc.netlify.app/',
     sourceLink: 'https://github.com/orlandodiazc/mathematicians',
   },
   {
     name: 'Todo List',
-    image: '../images/portfolio/spacetravelers.png',
+    image: '../images/portfolio/todolist.png',
     description:
-      'To do list is JavaScript project that allows you to add, delete and edit your daily tasks.',
+      'To-do List es un proyecto de JavaScript que permite agregar, eliminar y editar tareas diarias.',
     tech: ['Javascript', 'CSS', 'HTML5', 'Webpack'],
     liveLink: 'https://stunning-bonbon-dbeffd.netlify.app/',
     sourceLink: 'https://github.com/orlandodiazc/to-do-list',
   },
 ];
+
 function displayProjects(projects) {
   projects.forEach((proj, i) => {
     const projClone = workTemplate.content.cloneNode(true);
